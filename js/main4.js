9c76bf9d2b3f9d90db4f9c81786b9edf0e2286f9
Studio42-com <studio42dotcom@gmail.com>
2021-11-29 11:55:01 -0800

    11.29.2021 12.00 with win-lose counter issues

--- js/main4.js
+++ js/main4.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/
 const database = ["pug", "icon", "crate", "orange", "lobster", "pyromaniac"];
 
-//Selects random word and calculates length.
+/*----- app's state (variables) -----*/
 var randomArray; //the selected word converted to an array of single letters.
 var lettersArray; //displays correctly guessed letters
 var remainingLetters; //how many letters left, based on random.length.
@@ -9,10 +9,22 @@ var random; //randomly selected word
 let guessedLetters =[]; //array for letters guessed, display.
 let winCounter; //if letters left and countdown = 0, declare win.
 let loseCounter; //if = 0, game over.
+let inputData; //data from user input.
+let wordIndex;
 
-/*----- app's state (variables) -----*/
-// let scores, results, winner;
-function getRandom(database){
+
+//console.log(random,random.length);
+// console.log(lettersArray, remainingLetters, random);
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+//document.querySelector('button').addEventListener('click', gamePlay); //adjust
+//document.querySelector('button').addEventListener('click', init); //is working.
+
+/*----- functions -----*/
+
+function getRandom(database){ //select word, build arrays, determine letters count.
     random = database[Math.floor(Math.random() * database.length)];
     //this supports the random selections if I add/subtract array items without code modifications except to databse)
     randomArray = Array.from(random);
@@ -21,38 +33,19 @@ function getRandom(database){
     for (var i = 0; i < random.length; i++) {
       lettersArray[i] = "_";
     }
-//    remainingLetters = random.length;
+    console.log("Randomization Complete");
 };
-    let inputData; //data from user input
 
-//console.log(random,random.length);
-// console.log(lettersArray, remainingLetters, random);
-
-/*----- cached element references -----*/
-
-/*----- event listeners -----*/
-document.querySelector('table').addEventListener('click', handleMove);
-document.querySelector('button').addEventListener('click', init);
-
-
-//button click listener to start/restart game
-//get input from text field and button and invoke gamePlay
-
-//need a PLAY AGAIN button to invoke INIT?
-
-//init();
-
-/*----- functions -----*/
-
- function init(){
+ function init(){ //initialize and reset for new game.
     getRandom(database);
     loseCounter = 6;
     winCounter = random.length;
     guessedLetters = [];
+    wordIndex;
     //console.log(lettersArray, remainingLetters, random, loseCounter,winCounter,guessedLetters);
-    
-    render();
     console.log("I just ran INIT");
+    render();
+    
 };
 
 function getInputValue(){
@@ -62,20 +55,25 @@ function getInputValue(){
     // Displaying the value
     alert(inputData);
     console.log("The Selected Letter is: ",inputData);
-    gamePlay;
-}
+    console.log("Completed getting user input");
+    gamePlay(randomArray,lettersArray, guessedLetters, wordIndex, winCounter,loseCounter, inputData);
+    console.log("preparing to run gamePlay");   
+};
 /* -------------------gamePlay function ----------------*/
-gameplay(randomArray,lettersArray, guessedLetters, winCounter, loseCounter, inputData);
-function gameplay(randomArray,lettersArray, guessedLetters, winCounter,loseCounter, inputData) {
-let indexArray = randomArray.indexOf(inputData); {
-    if (indexArray > -1) {
-        winCounter -= 1;
-        lettersArray.splice(indexArray,1,inputData);
+//function gamePlay(randomArray,lettersArray, guessedLetters, winCounter, loseCounter, inputData);
+function gamePlay(randomArray,lettersArray, guessedLetters, wordIndex, winCounter,loseCounter, inputData) {
+    console.log("Starting gamePlay function");
+    wordIndex = randomArray.indexOf(inputData); {
+    if (wordIndex > -1) {
+        (winCounter -= 1);
+        console.log("winCounter should be less 1")
+        lettersArray.splice(wordIndex,1,inputData);
         function goodGuess(){
             alert ("Good Letter choice!");
             }
             } else {
-                loseCounter -=1;
+                (loseCounter -=1);
+                console.log("loseCounter should be less 1")
                 guessedLetters.push(inputData);
                 function badGuess(){
                     alert ("You have chosen incorrectly");
@@ -83,30 +81,23 @@ let indexArray = randomArray.indexOf(inputData); {
 
                 }
     }
-// console.log("Index: ",indexArray, "WinCounter: ",winCounter, "LoseCounter: ",loseCounter);
-// console.log("Valid Letters :", lettersArray)
-// console.log("Bad letters: ", guessedLetters);
-console.log(lettersArray);
+console.log("Index: ",wordIndex, "WinCounter: ",winCounter, "LoseCounter: ",loseCounter);
+console.log("Valid Letters :", lettersArray)
+console.log("Bad letters: ", guessedLetters);
+console.log("gamePlay Function has completed.");
+    render();
 };
-        
-    //if letter is in chosen word array:
-        //inject letter into successful guesses array at the index location.
-                //would like this to work if same letter in word multiple times
-        // subtract 1 from "letters remaining counter". If can do multiple letters, -1/letter
-        // if remaining guesses =0, declare winner
-
-    //if letter NOT in chosen word array:
-        // -1 from bad guesses number
-        // if bad guesses number = 0, declare "game over"
 
 /*------------------- End gamePlay function --------------------*/
 
 function render(){
+    console.log("post-render report: Index: ",wordIndex, "WinCounter: ",winCounter, "LoseCounter: ",loseCounter);
     document.getElementById("guessWord").innerHTML = lettersArray; //remove the comas later.
     document.getElementById("showRandom").innerHTML = randomArray; //remove the comas later,remove later.
 
     document.getElementById("badGuesses").innerHTML = loseCounter;
     document.getElementById("remainingLetters").innerHTML = winCounter;
     document.getElementById("guessed").innerHTML = guessedLetters;
+    console.log("post-render report: Index: ",wordIndex, "WinCounter: ",winCounter, "LoseCounter: ",loseCounter);
     console.log("I have run Render");
 };
