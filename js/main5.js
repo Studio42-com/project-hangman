a0504bb027d834c7bef6ea378ffe17e78ad12b86
Studio42-com <studio42dotcom@gmail.com>
2021-11-29 21:19:55 -0800

    Success with graphics. Success with alerts converted to messages.

--- js/main5.js
+++ js/main5.js
@@ -1,5 +1,24 @@
 /*----- constants -----*/
 const database = ["pug", "icon", "crate", "orange", "lobster", "pyromaniac"];
+const image6 ="images/6.jpg";
+const image5 ="images/5.jpg";
+const image4 ="images/4.jpg";
+const image3 ="images/3.jpg";
+const image2 ="images/2.jpg";
+const image1 ="images/1.jpg";
+const image0 ="images/0.jpg";
+
+
+const lookup = {
+   "6" : image6,
+    "5": image5,
+    "4": image4,
+    "3": image3,
+    "2": image2,
+    "1": image1,
+    "0": image0
+};
+
 
 /*----- app's state (variables) -----*/
 var randomArray; //the selected word converted to an array of single letters.
@@ -53,7 +72,9 @@ function getInputValue(){
 //Check for duplicates
     let dupCheck = guessedLetters.indexOf(inputData); {
         if (dupCheck > -1) {
-            alert("Select something else");
+            // alert("Select something else");
+            document.getElementById("messages").innerHTML = "Select something else";
+
             getInput.value = "";
             return;
         }
@@ -80,28 +101,34 @@ function gamePlay() {
         lettersArray.splice(wordIndex,1,inputData);
         guessedLetters.push(inputData); //all guessed characters go into guessedLetters array.
                                         // so we can query if a character has been re-used.
-        goodGuess;
+        goodGuess();
         function goodGuess(){
-            alert ("Good Letter choice!");
+            // alert ("Good Letter choice!");
+            document.getElementById("messages").innerHTML = "Good Choice!";
+            
             }
             } else {
                 (loseCounter -=1);
                 console.log("loseCounter should be less 1")
                 guessedLetters.push(inputData);
-                badGuess;
+                badGuess();
                 function badGuess(){
-                    alert ("You have chosen incorrectly");
+                    // alert ("You have chosen incorrectly");
+                    document.getElementById("messages").innerHTML = "You have chosen incorrectly";
+
                     }
 
                 }
     }
     console.log("checking winner/loser");
     if (winCounter == 0 ){
-        alert('You won!');
+        // alert('You won!');
+        document.getElementById("messages").innerHTML = "You Won!";
     
     }
     if (loseCounter == 0 ){
-        alert("Uh, yeah, that didn't exactly end favorably");
+        // alert("Uh, yeah, that didn't exactly end favorably");
+        document.getElementById("messages").innerHTML = "That didn't end well....";
         
     }
 
@@ -124,7 +151,7 @@ function render(){
     document.getElementById("guessed").innerHTML = guessedLetters;
     console.log("post-render report: Index: ",wordIndex, "WinCounter: ",winCounter, "LoseCounter: ",loseCounter);
     getInput.value = "";
-    document.getElementById("picture").innerHTML ="<img src=\'images/0.jpg\' border=1 width=25% height=25%>";
+    document.getElementById("picture").innerHTML = `<img src="${lookup[loseCounter]}" width=25% height=25% border=1>`;
     console.log("I have run Render");
 };
 
